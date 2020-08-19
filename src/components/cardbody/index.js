7ee130b899c383aa85dcc0c82472351023ff4e17
JCSR <johnconnorsryan@gmail.com>
2020-08-18 21:29:00 -0400

    got cardbody and navbar state to communicate through app.js

--- src/components/cardbody/index.js
+++ src/components/cardbody/index.js
@@ -1,16 +1,14 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Sub1 from './sub1';
 import Sub2 from './sub2';
 import Sub3 from './sub3';
 import Sub4 from './sub4';
 import Sub5 from './sub5';
-import Navbar from '../navbar'
+//import Navbar from '../navbar'
 
-function CardBody() {
-    const [currentPage, handlePageChange] = useState('About');
-     
-    /* const renderCard = () => {
-          switch(currentPage){
+function CardBody(props) {
+     function renderCard (tab)  {
+          switch(tab){
             case 'Sub1':
                 return <Sub1/>
             case 'Sub2':
@@ -24,28 +22,12 @@ function CardBody() {
                 default:
                   return <Sub1/>
         }
-    } */
+    } 
     return(
         <div className="container">
             <p>parent</p>
             <div classname="cardContainer">
-               {(() => {
-                   switch(currentPage){
-                    case 'Sub1':
-                        return <Sub1/>
-                    case 'Sub2':
-                        return <Sub2/>
-                    case 'Sub3':
-                        return <Sub3/>
-                    case 'Sub4':
-                        return <Sub4/>
-                    case 'Sub5':
-                        return <Sub5/>
-                        default:
-                          return <Sub1/>
-                   }
-               })()}
-                
+              {renderCard(props.currentPage)}
             </div>
              </div>
     )
