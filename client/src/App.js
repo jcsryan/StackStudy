35269991da9b55d078322d71df584948733c228c
Israel Berlanga <israel0688@hotmail.com>
2020-08-21 20:13:14 -0500

    cleaned up the code

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,38 @@
 import React, {useState} from 'react';
-import './App.css';
-import Navbar from './components/navbar'
-import CardBody from '../src/components/cardbody';
-import Login from './components/login'
-
+// add these two library import statements
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
+import { ApolloProvider as ApolloHooksProvider } from '@apollo/react-hooks'
+//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-
+import Navbar from './pages/navbar'
+import Cardbody from './pages/Cardbody'
 const client = new ApolloClient({
+  
+  request: operation => {
+    const token = localStorage.getItem('id_token');
 
-  uri: '/graphql',
-})
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
+
+
 
 function App() {
-  const [currentPage, handlePageChange] = useState('Sub1');
-
+  const [currentPage, handlePageChange] = useState('Login');
   return (
-    <ApolloProvider client={client}>
-      <Navbar currentPage={currentPage} handlePageChange={handlePageChange}></Navbar>
-      <CardBody currentPage={currentPage} handlePageChange={handlePageChange}></CardBody>  
+    <ApolloProvider client = {client}>
+      <ApolloHooksProvider client={client}>
+        <Navbar currentPage={currentPage} handlePageChange={handlePageChange}></Navbar>
+      <Cardbody currentPage={currentPage} handlePageChange={handlePageChange}></Cardbody>
+      </ApolloHooksProvider>
     </ApolloProvider>
-  ); 
+  );
 }
 
 export default App;
-
-
-/* 
-
-
-*/
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,24 +1,5 @@
 import gql from 'graphql-tag';
 
-/*
-export const QUERY_CARD = gql`
-query Card($username: String) {
-    Card(username: $username) {
-        _id
-        frontText
-        backText
-    }
-}`;
-
-
-export const QUERY_USER = gql`
-query user($username: String!) {
-    user(username: $username) {
-        username
-        email
-    }
-}`;
-*/
 
 export const QUERY_USER = gql`
 query users{
